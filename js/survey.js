4bb47b4df7d4d9a745a69bd107cc6529d719965a
barrosdaniel <barros.daniel@outlook.com>
2023-07-17 09:32:06 +0800

    feature: Add email and phone fields.

--- js/survey.js
+++ js/survey.js
@@ -7,19 +7,27 @@ let paragraphText;
 // Form fields
 const studentIDField = document.querySelector("#studentID");
 const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
+const phoneField = document.querySelector("#phone");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 });
 
 function showResponses() {
+    // Get form values
     const studentID = studentIDField.value;
     const name = nameField.value;
+    const email = emailField.value;
+    const phone = phoneField.value;
+    // Display responses
     contentTitle.innerHTML = "Your data has been successfully submitted!";
-    form.parentNode.removeChild(form);
+    form.parentNode.removeChild(form); // Removes the form element
     appendMessage();
     appendStudentID(studentID);
     appendName(name);
+    appendEmail(email);
+    appendPhone(phone);
 }
 
 function appendParagraph(paragraphText) {
@@ -43,5 +51,15 @@ function appendStudentID(studentID) {
 
 function appendName(name) {
     paragraphText = document.createTextNode("Name: " + name);
+    appendParagraph(paragraphText);
+}
+
+function appendEmail(email) {
+    paragraphText = document.createTextNode("Email: " + email);
+    appendParagraph(paragraphText);
+}
+
+function appendPhone(phone) {
+    paragraphText = document.createTextNode("Phone: " + phone);
     appendParagraph(paragraphText);
 }
