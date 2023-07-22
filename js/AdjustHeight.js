5e5e08ee16ffd74d1df4ea6e9c71764284ab2779
Daniel Barros <daniel.barros@cqumail.com>
2023-07-23 00:35:21 +0800

    chore: Add project files' headers.

--- js/AdjustHeight.js
+++ js/AdjustHeight.js
@@ -1,3 +1,11 @@
+/*
+Central Queensland University
+COIT12207 - Internet Applications (2023 Term 2)
+Campus: External
+Assignment 1 - Social Innovation Survey
+Student ID: 12184305
+Student Name: Daniel Barros
+*/
 window.addEventListener('DOMContentLoaded', adjustHeight);
 
 function adjustHeight() {
