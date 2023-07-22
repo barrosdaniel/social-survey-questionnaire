53bac27d6fd40e5fb259b781b75fdd4f063cfb2c
Daniel Barros <daniel.barros@cqumail.com>
2023-07-22 12:41:44 +0800

    feature: Add responses page and initial fields up to q2. Fixed multiple bugs.

--- js/survey.js
+++ js/survey.js
@@ -1,65 +1,68 @@
-const container = document.querySelector("#container");
-const contentTitle = document.querySelector("#contentTitle");
-const form = document.querySelector("#form");
-const submitButton = document.querySelector("#submit-button");
-submitButton.addEventListener("click", showResponses);
-let paragraphText;
-// Form fields
-const studentIDField = document.querySelector("#studentID");
-const nameField = document.querySelector("#name");
-const emailField = document.querySelector("#email");
-const phoneField = document.querySelector("#phone");
+function submitResponsesButtonClick() {
+    // Add Results page boilerplate to page string
+    let documentString = "";
+    documentString += "<h1 class='title'>Survey of the use of AI in Computer Science Education </h1>";
+    documentString += "<div class='container' id='container'>";
+    documentString += "<h2 class='content-title' id='contentTitle'>Your data has been successfully submitted.</h2>";
+    documentString += "<p>Your input is valuable to us and will assist with shaping the future of Education. Please see your answers below:</p>";
+    documentString += "<p>";
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-});
+    // Add responses to page string
+    // Student ID
+    const studentIDField = document.getElementById("studentID");
+    const studentIDValue = studentIDField.value;
+    documentString += "Student ID: " + studentIDValue + "<br>";
+    // Student Name
+    const studentNameField = document.getElementById("name");
+    const studentNameValue = studentNameField.value;
+    documentString += "Student Name: " + studentNameValue + "<br>";
+    // Email Address
+    const emailAddressField = document.getElementById("email");
+    const emailAddressValue = emailAddressField.value;
+    documentString += "Email Address: " + emailAddressValue + "<br>";
+    
+    // Email Address
+    const phoneNumberField = document.getElementById("phone");
+    const phoneNumberValue = phoneNumberField.value;
+    documentString += "Email Address: " + phoneNumberValue + "<br>";
 
-function showResponses() {
-    // Get form values
-    const studentID = studentIDField.value;
-    const name = nameField.value;
-    const email = emailField.value;
-    const phone = phoneField.value;
-    // Display responses
-    contentTitle.innerHTML = "Your data has been successfully submitted!";
-    form.parentNode.removeChild(form); // Removes the form element
-    appendMessage();
-    appendStudentID(studentID);
-    appendName(name);
-    appendEmail(email);
-    appendPhone(phone);
-}
-
-function appendParagraph(paragraphText) {
-    const paragraph = document.createElement("p");
-    if (paragraphText.textContent != "Your input is important to us and will assist with shaping the future of Education. Please see your responses below:" ) { 
-        paragraph.style.marginBottom = "0px";
+    // Q1 radio buttons
+    const q1Field = document.getElementsByName("q1");
+    let q1Value = "";
+    for (let value of q1Field) {
+        if (value.checked) {
+            q1Value = value.value;
+        }
     }
-    paragraph.appendChild(paragraphText);
-    container.appendChild(paragraph);
+    documentString += "1. Have you used AI-powered tools to enhance your learning process or productivity in Computer Science coursework? " + q1Value + "<br>";
+    // Q2 checkboxes
+    const q2Field = document.getElementsByName("q2");
+    let q2Value = [];
+    for (let value of q2Field) {
+        if (value.checked) {
+            q2Value.push(value.value);
+        }
+    }
+    documentString += "2. Which AI tools have you found most useful for enhancing your learning process or productivity in Computer Science coursework? " + q2Value.toString() + "<br>";
+
+
+
+
+
+    documentString += "<\p>";
+
+    // Add closing tags to page string
+    documentString += "<h3>Thank you for taking the time to complete our survey!</h3>";
+    documentString += "<\div>";
+
+    // Write page string to document and apply global stylesheet
+    document.write(documentString);
+    applyStylesheet();
 }
 
-function appendMessage() {
-    paragraphText = document.createTextNode("Your input is important to us and will assist with shaping the future of Education. Please see your responses below:");
-    appendParagraph(paragraphText);
-}
-
-function appendStudentID(studentID) {
-    paragraphText = document.createTextNode("Student ID: " + studentID);
-    appendParagraph(paragraphText);
-}
-
-function appendName(name) {
-    paragraphText = document.createTextNode("Name: " + name);
-    appendParagraph(paragraphText);
-}
-
-function appendEmail(email) {
-    paragraphText = document.createTextNode("Email: " + email);
-    appendParagraph(paragraphText);
-}
-
-function appendPhone(phone) {
-    paragraphText = document.createTextNode("Phone: " + phone);
-    appendParagraph(paragraphText);
+function applyStylesheet() {
+    let styleSheetLink = document.createElement("link");
+    styleSheetLink.rel = "stylesheet";
+    styleSheetLink.href = "css/styles.css";
+    document.head.appendChild(styleSheetLink);
 }
