3173e55f223ae6e9c269023a0054142176183c86
Daniel Barros <daniel.barros@cqumail.com>
2023-07-22 14:10:03 +0800

    feature: Add questions 3 - 7.

--- js/survey.js
+++ js/survey.js
@@ -35,6 +35,7 @@ function submitResponsesButtonClick() {
         }
     }
     documentString += "1. Have you used AI-powered tools to enhance your learning process or productivity in Computer Science coursework? " + q1Value + "<br>";
+
     // Q2 checkboxes
     const q2Field = document.getElementsByName("q2");
     let q2Value = [];
@@ -44,6 +45,51 @@ function submitResponsesButtonClick() {
         }
     }
     documentString += "2. Which AI tools have you found most useful for enhancing your learning process or productivity in Computer Science coursework? " + q2Value.toString() + "<br>";
+
+    // Q3 radio buttons
+    const q3Field = document.getElementsByName("q3");
+    let q3Value = "";
+    for (let value of q3Field) {
+        if (value.checked) {
+            q3Value = value.value;
+        }
+    }
+    documentString += "3. Do you think AI technology has positively impacted your learning process or productivity in Computer Science coursework? " + q3Value + "<br>";
+
+    // Q4 radio buttons
+    const q4Field = document.getElementsByName("q4");
+    let q4Value = "";
+    for (let value of q4Field) {
+        if (value.checked) {
+            q4Value = value.value;
+        }
+    }
+    documentString += "4. Have you used any AI-based code analysis or debugging tools? " + q4Value + "<br>";
+    
+    // Q5 radio buttons
+    const q5Field = document.getElementsByName("q5");
+    let q5Value = "";
+    for (let value of q5Field) {
+        if (value.checked) {
+            q5Value = value.value;
+        }
+    }
+    documentString += "5. How have AI tools impacted your ability to collaborate and engage in peer learning experiences within the context of Computer Science studies? " + q5Value + "<br>";
+
+    // Q6 textarea
+    const q6Field = document.getElementById("q6");
+    const q6Value = q6Field.value;
+    documentString += "6. Please provide details of how AI tools impacted your ability to collaborate and engage in peer learning experiences within the context of Computer Science studies. " + q6Value + "<br>";
+
+    // Q7 radio buttons
+    const q7Field = document.getElementsByName("q7");
+    let q7Value = "";
+    for (let value of q7Field) {
+        if (value.checked) {
+            q7Value = value.value;
+        }
+    }
+    documentString += "7. Have you experienced any limitations or drawbacks when using AI for enhancing your learning process or productivity in Computer Science coursework? " + q7Value + "<br>";
 
 
 
